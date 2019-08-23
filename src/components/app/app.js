0e9f8fb8fdf5a2f4ff39380873a7cc7c808fd92d
viktar hameza <viktor.hameza@gmail.com>
2019-08-23 20:45:43 +0300

    componentDidMount(

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,22 +7,27 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends React.Component {
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  onItemSelected = (id) => {
+    console.log(id)
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onItemSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -30,7 +30,7 @@ export default class ItemList extends Component {
       return(
           <li className="list-group-item" 
             key={id}
-             onClick={this.onClick} >
+             onClick={() => this.props.onItemSelected(id)} >
             {name}
           </li>
       )
